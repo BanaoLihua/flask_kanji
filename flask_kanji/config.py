cbbd9d8d8bfafaebb2fdc35e96808ad20b6df8f2
banao <tatitachi870@gmail.com>
2022-05-28 22:34:21 +0900

    rewrite

--- flask_kanji/config.py
+++ flask_kanji/config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = 'postgres://gvvhwaiqjtqjps:3d74c5f4c4a222a3678ee5c7b4564a60b946e985f945e05ee5313dafe5d1a598@ec2-44-196-174-238.compute-1.amazonaws.com:5432/d83551d7nilojp'
+SQLALCHEMY_DATABASE_URI = 'postgresql://gvvhwaiqjtqjps:3d74c5f4c4a222a3678ee5c7b4564a60b946e985f945e05ee5313dafe5d1a598@ec2-44-196-174-238.compute-1.amazonaws.com:5432/d83551d7nilojp'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 DEBUG = True
